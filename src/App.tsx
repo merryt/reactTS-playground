184d2094cf002768268a9c8dcc58853dfaf8398e
merryt <tymerry@gmail.com>
2022-05-19 20:28:35 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import InputField from './components/InputField';
+// import TodoList from './components/TodoList';
+import { Todo } from './model';
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("") 
+  const [todos, setTodos] = useState<Todo[]>([]) 
+
+  const handleAdd = (e : React.SyntheticEvent) =>{
+    e.preventDefault()
+    if(todos){
+      setTodos([...todos, {id: Date.now(),todo: todo, isDone: false }]);
+    }
+    setTodo("")
+  }
+
+  console.log(todos)
+  console.log(todo, "test")
   return (
     <div className="App">
       <span className="heading">Taskify</span>
 
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      {/* <TodoList todos={todos}/> */}
+      {todos.map(t=> <span>{t.todo}</span>)}
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./InputField.css"
 
-export default function InputField() {
+interface Props{
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.SyntheticEvent)=> void;
+}
+
+
+export default function InputField({todo, setTodo, handleAdd}: Props) {
   return (
-    <form className="input">
-        <input type="input" placeholder='Enter a task' className='input__box'/>
+    <form className="input" onSubmit={ handleAdd }>
+        <input type="input" placeholder='Enter a task' className='input__box'
+        value={todo}
+        onChange={
+          (e)=> setTodo(e.target.value)
+        }/>
         <button className="input__submit" type="submit">go</button>
     </form>
   )
